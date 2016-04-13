077a63d2b4c5d93f0071f2e1f3b25d5fabe75001
Luis De Siqueira <louthebrew@gmail.com>
2016-04-13 14:17:20 -0700

    adding package action to fpm resources

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'Installs/Configures fpm'
 long_description 'Installs/Configures fpm'
-version '0.1.0'
+version '0.1.1'
 
 depends 'rbenv'
 depends 'poise'
