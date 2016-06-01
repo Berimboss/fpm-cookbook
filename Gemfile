92900e563d8d3d0ba5c3e9da45299db2a75e6b6a
LouTheBrew <louthebrew@gmail.com>
2016-05-31 18:11:36 -0700

    TECHOPS-2303 changing public key

--- Gemfile
+++ Gemfile
@@ -2,4 +2,6 @@ source 'https://rubygems.org'
 
 gem "test-kitchen"
 gem "kitchen-vagrant"
+gem "kitchen-ec2"
+gem "berkshelf"
 gem "kitchen-inspec"
--- libraries/fpm.rb
+++ libraries/fpm.rb
@@ -23,6 +23,58 @@ module Fpm
   class Provider < Chef::Provider
     include Poise
     provides :fpm
+    def menu
+      {
+        :items => [
+          {:symbol => '-t', :input => 'OUTPUT_TYPE', :description => 'the type of package you want to create (deb, rpm, solaris, etc)', :type => "String"},
+          {:symbol => '-s', :input => 'INPUT_TYPE', :description => 'the package type to use as input (gem, rpm, python, etc)', :type => "String"},
+          {:symbol => '-C', :input => 'CHDIR', :description => 'Change directory to here before searching for files', :type => "String"},
+          {:symbol => '--prefix', :input => 'PREFIX', :description => 'A path to prefix files with when building the target package. This may be necessary for all input packages. For example the gem type will prefix with your gem directory automatically', :type => "String"},
+          {:symbol => '-p', :input => 'OUTPUT', :description => 'The package file path to output', :type => "String"},
+          {:symbol => '-f', :input => 'FORCE', :description => 'Force output even if it will overwrite an existing file', :type => "BOOL", default: true},
+          {:symbol => '-n', :input => 'NAME', :description => 'The name to give to the package', :type => "String"},
+          {:symbol => '--log', :input => 'LOG', :description => 'Set the log level, Values: error, warn, info, debug', :type => "String", :possibles => %w{error warn info debug}},
+          {:symbol => '--verbose', :input => 'VERBOSE', :description => 'Enable verbose output', :type => "String", default: false},
+          {:symbol => '--debug', :input => 'DEBUG', :description => 'Enable debug output', :type => "String"},
+          {:symbol => '--debug-workspace', :input => 'DEBUGWORKSPACE', :description => 'Keep any file workspaces around for debugging', :type => "String"},
+          {:symbol => '-v', :input => 'VERSION', :description => 'The version to give to the package', :type => "String", default: '1.0'},
+          {:symbol => '--iteration', :input => 'ITERATION', :description => 'The iteration to give to the package. RPM calls this the release.', :type => "String"},
+          {:symbol => '--epoch', :input => 'EPOCH', :description => 'The epoch value for this package. RPM and debian calls this epoch', :type => "String"},
+          {:symbol => '--license', :input => 'LICENSE', :description => '(optional) license name for this package', :type => "String"},
+          {:symbol => '--vendor', :input => 'VENDOR', :description => '(optional) vendor name for this package', :type => "String"},
+          {:symbol => '--category', :input => 'CATEGORY', :description => '(optional) category this package belongs to', :type => "String", default: 'none'},
+          {:symbol => '-d', :input => 'DEPENDENCY', :description => 'a dependency. this flag can be specified multiple times', :type => "String", default: 'none'},
+          {:symbol => '--no-depends', :input => 'DEPENDENCY', :description => 'Do not list any dependencies in this package', :type => "String", default: false},
+          {:symbol => '--no-auto-depends', :input => 'DEPENDENCY', :description => 'Do not list any dependencies in this package automatically', :type => "String", default: false},
+          {:symbol => '--provides', :input => 'PROVIDES', :description => 'What this package provides, can be specified multiple times', :type => "String", default: false},
+          {:symbol => '--conflicts', :input => 'CONFLICTS', :description => 'Other packages/versions this package conflicts with, this flag can be specified multiple times', :type => "String", default: false},
+          {:symbol => '--replaces', :input => 'REPLACES', :description => 'Other packages/versions this package replaces. this flag can be specified multiple times', :type => "String", default: false},
+          {:symbol => '--config-files', :input => 'CONFIG_FILES', :description => 'Mark a file in the package as being a config file. can be multiple and directory recursive', :type => "String", default: false},
+          {:symbol => '--directories', :input => 'DIRECTORIES', :description => 'Recursively mark a directory as being owned by the package', :type => "String"},
+          {:symbol => '-a', :input => 'ARCHITECTURE', :description => 'the architecture name. usually matches uname -m', :type => "String"},
+          {:symbol => '-m', :input => 'MAINTAINER', :description => 'the architecture name. usually matches uname -m', :type => "String"},
+          {:symbol => '-S', :input => 'PACKAGE_NAME_SUFFIX', :description => 'a name suffix to append to package and dependencies', :type => "String"},
+          {:symbol => '-e', :input => 'EDIT', :description => 'Edit the package spec before building', :type => "BOOL", default: false},
+          {:symbol => '-x', :input => 'EXCLUDE_PATTERN', :description => 'Exclude paths matching pattern (shell wildcard globs valid here), can be multiple', :type => "BOOL", default: false},
+          {:symbol => '--description', :input => 'DESCRIPTION', :description => 'Exclude paths matching pattern (shell wildcard globs valid here), can be multiple', :type => "BOOL", default: false},
+          {:symbol => '--url', :input => 'URI', :description => 'add a url for this package', :type => "BOOL", default: false},
+          {:symbol => '--inputs', :input => 'INPUTS_PATH', :description => 'The path to a file containing a newline-separated list of files and dirs to use as input'},
+          {:symbol => '--after-install', :input => 'FILE', :description => 'A script to be run after package installation'},
+          {:symbol => '--before-install', :input => 'FILE', :description => 'A script to be run before package installation'},
+          {:symbol => '--after-remove', :input => 'FILE', :description => 'A script to be run after package removal'},
+          {:symbol => '--before-remove', :input => 'FILE', :description => 'A script to be run before package removal'},
+          {:symbol => '--after-upgrade', :input => 'FILE', :description => 'A script to be run after package upgrade'},
+          {:symbol => '--before-upgrade', :input => 'FILE', :description => 'A script to be run before package upgrade'},
+          {:symbol => '--template-scripts', :input => 'FILE', :description => 'allow scripts to be templated this lets you use ERB to template your packaging scripts'},
+          {:symbol => '--template-value', :input => 'KEY=VALUE', :description => 'make key avail in script templates so <%=key%> works'},
+          {:symbol => '--work-dir', :input => 'KEY=VALUE', :description => 'make key avail in script templates so <%=key%> works'},
+          {:symbol => '--gem-bin-path', :input => 'DIRECTORY', :description => '(gem only) The directory to install gem executables'},
+          {:symbol => '--gem-package-name-prefix', :input => 'PREFIX', :description => '(gem only) The directory to install gem executables'},
+          {:symbol => '--gem-gem', :input => 'PATH_TO_GEM', :description => 'The path to the gem tool'},
+          {:symbol => '-h', :input => 'HELP', :description => 'The path to the gem tool'},
+        ]
+      }
+    end
     def output_name(name, ext='rpm', arch='x86_64', version='1.0')
       "#{name}-#{version}.#{arch}.#{ext}"
     end
