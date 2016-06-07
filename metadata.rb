1ae45ae2785d1a3ef2aee9b7d190d4dbd0f72064
LouTheBrew <louthebrew@gmail.com>
2016-06-07 05:54:20 -0700

    ldesiqueira

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'Installs/Configures fpm'
 long_description 'Installs/Configures fpm'
-version '0.1.11'
+version '0.1.12'
 
 depends 'rbenv'
 depends 'poise'
