fd2c5fb348084db9127a12675dd0d04cd3d74a89
LouTheBrew <louthebrew@gmail.com>
2016-06-07 09:08:46 -0700

    updated blindly from rake to version 0.1.13

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name 'poise-fpm'
+name ::File.read('NAME').strip
 maintainer 'Luis De Siqueira'
 maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'Installs/Configures fpm'
 long_description 'Installs/Configures fpm'
-version '0.1.12'
+version ::File.read('VERSION').strip
 
 depends 'rbenv'
 depends 'poise'
--- publish.sh
+++
@@ -1,1 +0,0 @@
-knife cookbook site share poise-fpm "Package Management" -o ~/.work/github.com/Berimboss/
